1e69e3cd33965584793cfc330eb1ce31de71b74d
Lư Thăng <0921030161@hiast.edu.vn>
2024-09-10 16:32:14 +0700

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -2,14 +2,14 @@ var VERSION = "0.0.7";
 var showVersionChanges = false;
 
 var sessions;
-var activeSession;
-var backupData;
-var refreshSettings;
-var showProfileNumber;
+var activeSession;		//Which profile is in use? Points to the array location of sessions
+var backupData;                 //Contains all undo actions {id,type,date,data}
+var refreshSettings;            //Values: "0"-No Update / "1"-Update Current Tab / "2"-Update all tabs in current window / "3"-Update all tabs of all windows
+var showProfileNumber           //True: shows in the browser's icon a number representing the active profile
 var showProfileChooser;
-var integrateETC;
+var integrateETC;		//Boolean - Integrate with the extension 'Edit This Cookie'?
 var isInstalledETC;
-var activateShortcuts;
+var activateShortcuts;          //Boolean - Shortcuts are enabled? (It comprehends js injection)
 var justUpdated;
 var firstRun;
 var installDate;
@@ -19,115 +19,139 @@ var swapMyCookiesID = "dffhipnliikkblkhpjapbecpmoilcama";
 var forgetMeID = "gekpdemielcmiiiackmeoppdgaggjgda";
 
 var ls = {
-    set: function(name, value) {
-        return new Promise((resolve) => {
-            chrome.storage.local.set({ [name]: value }, function() {
-                resolve(value);
-            });
-        });
+    set : function(name, value) {
+        var toReturn = this.get(name);
+        localStorage.setItem(name, JSON.stringify(value));
+        return toReturn;
     },
-    get: function(name) {
-        return new Promise((resolve) => {
-            chrome.storage.local.get([name], function(result) {
-                resolve(result[name] || null);
-            });
-        });
+    get : function(name) {
+        var toReturn;
+        try {
+            toReturn = JSON.parse(localStorage.getItem(name));
+        } catch(e) {
+            toreturn = null;
+        }
+        return toReturn;
     },
-    remove: function(name) {
-        return new Promise((resolve) => {
-            chrome.storage.local.remove([name], function() {
-                resolve();
-            });
-        });
+    remove : function(name) {
+        var toReturn = this.get(name);
+        localStorage.removeItem(name);
+        return toReturn;
     }
-};
+}
 
-var updateData = async function() {
-    sessions = await ls.get("data_sessions");
-    
-    if (!sessions || sessions.length === 0) {
-        sessions = [
-            {
-                "name": "New Profile #0",
-                "cookies": [],
-                "created": new Date(),
-                "lastUsed": undefined
-            },
-            {
-                "name": "New Profile #1",
-                "cookies": [],
-                "created": new Date(),
-                "lastUsed": undefined
-            },
-            {
-                "name": "New Profile #2",
-                "cookies": [],
-                "created": new Date(),
-                "lastUsed": undefined
-            }
-        ];
+var updateData = function() {
+    sessions = ls.get("data_sessions");
+    //sessions = null;
+    if(sessions == null || sessions == undefined) {
+        sessions = new Array();
+        sessions[0] = {
+            "name":"New Profile #0",
+            "cookies":new Array(),
+            "created":new Date(),
+            "lastUsed": undefined
+        };
+        sessions[1] = {
+            "name":"New Profile #1",
+            "cookies":new Array(),
+            "created":new Date(),
+            "lastUsed": undefined
+        };
+        sessions[2] = {
+            "name":"New Profile #2",
+            "cookies":new Array(),
+            "created":new Date(),
+            "lastUsed": undefined
+        };
+    } else if(sessions.length == 0) {
+        sessions = new Array();
+        sessions[0] = {
+            "name":"New Profile #0",
+            "cookies":new Array(),
+            "created":new Date(),
+            "lastUsed": undefined
+        };
     }
 
-    activeSession = await ls.get("data_activeSession");
-    if (activeSession == null || activeSession >= sessions.length || activeSession < 0) {
+    activeSession = ls.get("data_activeSession");
+    if(activeSession == null || activeSession >= sessions.length || activeSession < 0 || activeSession == undefined) {
         activeSession = 0;
-    }
+    }	
     sessions[activeSession].lastUsed = new Date();
 
-    backupData = await ls.get("data_backupData");
-    if (!backupData) {
-        backupData = [];
+    backupData = ls.get("data_backupData");
+    if(backupData == null || backupData == undefined) {
+        backupData = new Array();
     }
 
-    refreshSettings = await ls.get("options_refreshSettings");
-    if (refreshSettings == null) {
+    refreshSettings = ls.get("options_refreshSettings");
+    if(refreshSettings == null || refreshSettings == undefined) {
         refreshSettings = 2;
     }
 
-    showProfileNumber = await ls.get("options_showProfileNumber");
-    if (showProfileNumber == null) {
+    showProfileNumber = ls.get("options_showProfileNumber");
+    if(showProfileNumber == null || showProfileNumber == undefined) {
         showProfileNumber = true;
     }
 
-    showProfileChooser = await ls.get("options_showProfileChooser");
-    if (showProfileChooser == null) {
+    showProfileChooser = ls.get("options_showProfileChooser");
+    if(showProfileChooser == null || showProfileChooser == undefined) {
         showProfileChooser = false;
     }
 
-    activateShortcuts = await ls.get("options_activateShortcuts");
-    if (activateShortcuts == null) {
+    activateShortcuts = ls.get("options_activateShortcuts");
+    if(activateShortcuts == null || activateShortcuts == undefined) {
         activateShortcuts = false;
     }
-
-    integrateETC = await ls.get("options_integrateETC");
-    if (integrateETC == null) {
+	
+    integrateETC = ls.get("options_integrateETC");
+    if(integrateETC == null || integrateETC == undefined) {
         integrateETC = true;
     }
 
-    isInstalledETC = await ls.get("status_isInstalledETC");
-    if (isInstalledETC == null) {
+    isInstalledETC = ls.get("status_isInstalledETC");
+    if(isInstalledETC == null || isInstalledETC == undefined) {
         isInstalledETC = false;
     }
-
-    installDate = await ls.get("data_installDate");
-    if (!installDate) {
+    
+    installDate = ls.get("data_installDate");
+    if(installDate == null) {
         installDate = new Date();
-        await ls.set("data_installDate", installDate);
+        ls.set("data_installDate", installDate);
     }
 
-    var oldVersion = await ls.get("status_version");
-    firstRun = await ls.get("status_firstRun") === null;
+    var oldVersion = ls.get("status_version");
+    firstRun = ls.get("status_firstRun");
+    if(firstRun == null)
+        firstRun = true;
+    else
+        firstRun = false;
+    if(oldVersion == VERSION)
+        justUpdated = false;
+    else
+        justUpdated = true;
 
-    justUpdated = (oldVersion !== VERSION);
+    setTimeout ( function() {
+        ls.set("data_sessions", sessions);
+        ls.set("data_activeSession", activeSession);
+        ls.set("data_backupData",backupData);
+        ls.set("options_refreshSettings", refreshSettings);
+        ls.set("options_showProfileNumber", showProfileNumber);
+        ls.set("options_showProfileChooser", showProfileChooser);
+        ls.set("options_activateShortcuts", activateShortcuts);
+        ls.set("options_integrateETC", integrateETC);
+        ls.set("status_version", VERSION);
+        ls.set("data_installDate", installDate);
+        ls.set("status_firstRun", false);
+    },1,sessions,activeSession,refreshSettings,showProfileNumber,activateShortcuts,integrateETC,VERSION,installDate);
 
-    await ls.set("data_sessions", sessions);
-    await ls.set("data_activeSession", activeSession);
-    await ls.set("data_backupData", backupData);
-    await ls.set("options_refreshSettings", refreshSettings);
-    await ls.set("options_showProfileNumber", showProfileNumber);
-    await ls.set("options_showProfileChooser", showProfileChooser);
-    await ls.set("options_activateShortcuts", activateShortcuts);
-    await ls.set("options_integrateETC", integrateETC);
-    await ls.set("status_version", VERSION);
-    await ls.set("status_firstRun", false);
 };
+updateData();
+
+function triggerUpdateData() {
+    var tabs = chrome.extension.getViews();
+    for(var i=0; i<tabs.length; i++){
+        if (tabs[i].updateData)
+            tabs[i].updateData();
+    }
+}
--- js/popup.js
+++ js/popup.js
@@ -5,75 +5,79 @@ function setEvents() {
 		$(this).addClass('active');
 		$("#loading").show();
 		console.log("Sending switch request");
-		
-		// Sử dụng chrome.runtime.sendMessage thay vì chrome.extension.sendRequest
-		chrome.runtime.sendMessage({
+		chrome.extension.sendRequest({
 			"action": "switchProfile",
 			"switchTo": index
-		}, function() {
-			$("#loading").hide();
-			// Sử dụng location.reload thay vì window.location.reload
-			location.reload(); // Không cần tham số `true`, không dùng reload từ cache
-		});
+			}, function() {
+				$("#loading").hide();
+				window.location.reload(true);
+			}
+		);
 	});
-	
 	$("#options").click(function(){
-		chrome.tabs.query({}, function(tabs) { // chrome.tabs.query thay vì chrome.tabs.getAllInWindow
+		chrome.tabs.getAllInWindow(null, function tabSearch(tabs) {
 			for(var i=0; i<tabs.length; i++) {
 				var tab = tabs[i];
-				if(tab.url.indexOf(chrome.runtime.getURL('options.html')) >= 0) { // Sử dụng chrome.runtime.getURL
-					chrome.tabs.update(tab.id, { active: true }); // Sử dụng { active: true } thay vì { selected: true }
+				if(tab.url.indexOf(chrome.extension.getURL('options.html')) >= 0) {
+					chrome.tabs.update(tab.id, {
+						selected:true
+					});
 					return;
 				}
 			}
-			chrome.tabs.create({ url: chrome.runtime.getURL('options.html') });
+			chrome.tabs.create({
+				url:chrome.extension.getURL('options.html')
+			});
 		});
 	});
 }
 
 function showProfiles(callback) {
-	// Sử dụng chrome.runtime.sendMessage thay vì chrome.extension.sendRequest
-	chrome.runtime.sendMessage({ "action": "getProfiles" }, function(response) {
-		var profiles = response.profiles;
-		var activeSession = response.activeSession;
-		
-		$("#profilesList").empty();
-		for(var i=0; i<profiles.length; i++) {
-			try {
-				var currentName = profiles[i];
-				console.log(currentName);
-				
-				var line = $( document.createElement('div') );
-				line.addClass("session");
-				if (i == profiles.length - 1)
-					line.addClass("lastSession");
-				
-				var indexInput = $( document.createElement('input') );
-				indexInput.addClass("index");
-				indexInput.attr("type", "hidden");
-				indexInput.attr("value", i);
-				
-				var nameDiv = $( document.createElement('div') );
-				nameDiv.addClass("name button");
-				nameDiv.text(currentName);
-				
-				if(activeSession == i)
-					nameDiv.addClass("active");
-				else
-					nameDiv.addClass("unactive");
-				
-				line.append(indexInput);
-				line.append(nameDiv);
-				$("#profilesList").append(line);
-			} catch(err) {
-				console.error("Error while inserting profiles in list!\n" + err.message);
+	chrome.extension.sendRequest({ "action": "getProfiles" },
+		function(response) {
+			var profiles = response.profiles;
+			var activeSession = response.activeSession;
+			
+			$("#profilesList").empty();
+			for(var i=0; i<profiles.length; i++) {
+				try {
+					var currentName = profiles[i];
+					console.log(currentName);
+					
+					var line = $( document.createElement('div') );
+					line.addClass("session");
+					if (i == profiles.length - 1)
+						line.addClass("lastSession");
+					var indexInput = $( document.createElement('input') );
+					indexInput.addClass("index");
+					indexInput.attr("type","hidden");
+					indexInput.attr("value",i);
+					var nameDiv = $( document.createElement('div') );
+					nameDiv.addClass("name");
+					nameDiv.addClass("button");
+					nameDiv.text(currentName);
+					if(activeSession == i)
+						nameDiv.addClass("active");
+					else
+						nameDiv.addClass("unactive");
+					line.append(indexInput);
+					line.append(nameDiv);
+					$("#profilesList").append(line);
+				} catch(err) {
+					console.error("Error while inserting profiles in list!\n" + err.description);
+				}
 			}
-		}
-		callback();
+			
+			callback();
 	});
 }
 
-// Hiển thị phần loading
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 $("#loading").show();
 jQuery(document).ready(function(){
 	showProfiles(setEvents);
